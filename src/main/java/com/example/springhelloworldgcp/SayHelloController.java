229f9b3353db393ff858b3c7289308caec95c59f
Sandeep K S <Sandeep.KS@gds.ey.com>
2019-09-12 19:48:54 +0530

    first commit again

--- src/main/java/com/example/springhelloworldgcp/SayHelloController.java
+++ src/main/java/com/example/springhelloworldgcp/SayHelloController.java
@@ -1,4 +1,17 @@
 package com.example.springhelloworldgcp;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hellov1")
 public class SayHelloController {
+
+    @GetMapping
+    public String getMessage() {
+        return String.format("This is hello from pod 1");
+    }
 }
